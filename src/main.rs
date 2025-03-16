2db7c2b2ea27b12adad372fb44bf2ad1f4eec468
Ayato <ay4toh5i@gmail.com>
2025-03-16 15:29:03 +0900

    Make it possible to handle log level

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use colored::{ColoredString, Colorize};
+use serde_json::{Map, Value};
 use std::io;
-use colored::Colorize;
-use serde_json::{Value, Map};
 
 struct Keys<'a> {
     message: &'a str,
@@ -14,12 +14,34 @@ const KEYS: Keys = Keys {
     timestamp: "timestamp",
 };
 
-fn main() {
-    println!("Hello, world!");
+enum LogLevel {
+    Debug,
+    Info,
+    Warn,
+    Error,
+    Fatal,
+}
 
+struct Level<'a> {
+    debug: &'a str,
+    info: &'a str,
+    warn: &'a str,
+    error: &'a str,
+    fatal: &'a str,
+}
+
+static LEVEL: Level = Level {
+    debug: "DEBUG",
+    info: "INFO",
+    warn: "WARN",
+    error: "ERROR",
+    fatal: "FATAL",
+};
+
+fn main() {
     let lines = io::stdin().lines();
     for line in lines {
-        println!("{}: {}", "output".green(), pretty(&line.unwrap()));
+        println!("{}", pretty(&line.unwrap()));
     }
 }
 
@@ -31,9 +53,35 @@ fn pretty(line: &str) -> String {
             let message = get_value_as_string(&value, KEYS.message);
             let level = get_value_as_string(&value, KEYS.level);
             let timestamp = get_value_as_string(&value, KEYS.timestamp);
-            format!("{}: {} - {}", timestamp, level.green(), message)
+            format!("[{}] {} : {}", timestamp, colorize_level(level), message)
         }
         _ => line.to_string(),
+    }
+}
+
+fn level(level: &str) -> LogLevel {
+    if level == LEVEL.debug {
+        LogLevel::Debug
+    } else if level == LEVEL.info {
+        LogLevel::Info
+    } else if level == LEVEL.warn {
+        LogLevel::Warn
+    } else if level == LEVEL.error {
+        LogLevel::Error
+    } else if level == LEVEL.fatal {
+        LogLevel::Fatal
+    } else {
+        LogLevel::Info
+    }
+}
+
+fn colorize_level(l: String) -> ColoredString {
+    match level(l.as_str()) {
+        LogLevel::Debug => l.normal(),
+        LogLevel::Info => l.green(),
+        LogLevel::Warn => l.yellow(),
+        LogLevel::Error => l.red(),
+        LogLevel::Fatal => l.red().bold(),
     }
 }
 
